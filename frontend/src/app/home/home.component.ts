8b3d5ca4bfd640e5e59c440ace4b6ed048d61264
HugoErb <erb.hugo@yahoo.fr>
2024-08-02 15:11:08 +0200

    optimisation, imports, majs fixes

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,30 +4,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonService } from '../common.service';
 
-// Font Awesome Icons
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faWheelchair } from '@fortawesome/free-solid-svg-icons';
-import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
-import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
-import { faPersonWalkingArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
-
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private commonService: CommonService) { }
-  icons = { faLinkedin, faEnvelope, faWheelchair, faCalendarCheck, faGraduationCap, faUsers, faBookOpen, faPersonWalkingArrowRight, faCheck, faBars, faCode };
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, protected commonService: CommonService) { }
   burgerMenuOpened: boolean = false;
 
   // Variables concernants la page de formulaire de demande de formation
@@ -113,37 +97,6 @@ export class HomeComponent {
   toggleBurgerMenu(event: MouseEvent): void {
     event.stopPropagation();
     this.burgerMenuOpened = !this.burgerMenuOpened;
-  }
-
-  /**
-  * Extrait le nom de la formation à partir de l'élément HTML cliqué qui déclenche l'événement.
-  * Le nom est recherché dans un élément `<h3>` qui doit se trouver à l'intérieur du premier parent avec la classe 'rounded-lg' du point de clic.
-  * Si le nom est trouvé, la méthode redirige l'utilisateur vers le formulaire de formation associé au plan choisi.
-  *
-  * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode.
-  */
-  chooseTraining(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    const trainingName = target.closest('.rounded-lg')?.querySelector('h3')?.textContent?.trim();
-    if (trainingName) {
-      this.router.navigate(['/training-form', { chosenTrainingName: trainingName }]).then(() => {
-        window.scrollTo(0, 0);
-      });
-    } else {
-      console.error('Impossible de trouver le nom de la formation.');
-    }
-  }
-
-  /**
-  * Filtre et formate la saisie d'un numéro de téléphone dans un champ de saisie HTML.
-  * Seules les valeurs numériques sont conservées, et un espace est ajouté tous les deux chiffres.
-  * Limite la saisie à un maximum de 10 chiffres.
-  * 
-  * @param event L'événement d'entrée déclenché lors de la saisie dans le champ de saisie.
-  *              L'événement doit être de type `Event`.
-  */
-  formatAndRestrictPhoneInput(event: Event): void {
-    this.phoneNumberMail = this.commonService.formatAndRestrictPhoneInput(event);
   }
 
   /**
