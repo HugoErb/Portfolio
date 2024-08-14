ac165e0fb34ca6bf684a38e3a072cac6d84ba13c
HugoErb <erb.hugo@yahoo.fr>
2024-08-14 13:06:57 +0200

    création du composant icon card + adaptation des icones de fa dans le composant home

---
+++ frontend/src/app/components/icon-card/icon-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-icon-card',
+  templateUrl: './icon-card.component.html',
+  standalone: true,
+  imports: [CommonModule],
+})
+export class IconCardComponent {
+  @Input() iconClass: string = '';
+  @Input() label: string = '';
+  @Input() isSvg: boolean = false;
+}
