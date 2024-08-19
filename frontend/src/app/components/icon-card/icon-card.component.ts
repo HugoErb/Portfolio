c69d4359dcb85345ae66b21faf1a3d6445bc4534
HugoErb <erb.hugo@yahoo.fr>
2024-08-19 23:46:37 +0200

    refacto + ajout modal + ajout json

--- frontend/src/app/components/icon-card/icon-card.component.ts
+++ frontend/src/app/components/icon-card/icon-card.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommonService } from '../../common.service';
 
 @Component({
-  selector: 'app-icon-card',
-  templateUrl: './icon-card.component.html',
-  standalone: true,
-  imports: [CommonModule],
+    selector: 'app-icon-card',
+    templateUrl: './icon-card.component.html',
+    standalone: true,
+    imports: [CommonModule],
 })
 export class IconCardComponent {
-  @Input() iconClass: string = '';
-  @Input() label: string = '';
-  @Input() path: string = '';
-  @Input() isSvg: boolean = false;
-  @Input() viewBox: string = '';
+    constructor(protected commonService: CommonService) { }
+
+    @Input() iconClass: string = '';
+    @Input() label: string = '';
+    @Input() path: string = '';
+    @Input() isSvg: boolean = false;
+    @Input() viewBox: string = '';
 }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -4,140 +4,179 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonService } from '../common.service';
 import { IconCardComponent } from '../components/icon-card/icon-card.component';
+import { technoIcons } from '../../assets/data/technoIcons';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, FormsModule, IconCardComponent],
-  templateUrl: './home.component.html'
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule, FormsModule, IconCardComponent],
+    templateUrl: './home.component.html'
 })
+
+export interface Icon {
+    iconClass: string;
+    label: string;
+    description: string;
+}
+
 export class HomeComponent {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, protected commonService: CommonService) { }
-  burgerMenuOpened: boolean = false;
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, protected commonService: CommonService) { }
+    burgerMenuOpened: boolean = false;
 
-  // Variables concernants la page de formulaire de demande de formation
-  redirectionSection: string = "";
-  chosenTrainingName: string = "";
+    // Variables pour le mail
+    @ViewChildren('inputField') inputFields!: QueryList<ElementRef>;
+    public inputLabelMap = new Map<string, string>();
+    nameMail: string = "";
+    emailMail: string = "";
+    phoneNumberMail: string = "";
+    messageMail: string = "";
 
-  // Variables pour le mail
-  @ViewChildren('inputField') inputFields!: QueryList<ElementRef>;
-  public inputLabelMap = new Map<string, string>();
-  nameMail: string = "";
-  emailMail: string = "";
-  phoneNumberMail: string = "";
-  messageMail: string = "";
+    // Icônes de la partie "technologies"
+    icons = icons;
+    visibleIcons = [];
+    currentPage = 0;
+    iconsPerPage = 6;
 
-  ngOnInit() {
-
-    // On récupère le nom de la formation de la page home
-    if (this.activatedRoute.snapshot.params.hasOwnProperty('redirectionSection')) {
-      this.scrollToSection(this.activatedRoute.snapshot.params['redirectionSection']);
-    }
-  }
-
-  /**
-  * Navigue vers un composant spécifié.
-  *
-  * @param {string} composant - Le nom du composant vers lequel naviguer. Cela doit être le chemin ou
-  *                             la route associée au composant cible dans la configuration de routage Angular.
-  */
-  navigateTo(component: string) {
-    this.router.navigate([component]).then(() => {
-      window.scrollTo(0, 0);
-    });
-  }
-
-  /**
-  * Gère les clics à l'extérieur du menu burger pour fermer le menu.
-  * 
-  * Cette méthode est déclenchée par un écouteur d'événements qui surveille tous les clics dans le document.
-  * Si le menu burger est ouvert et que le clic n'est pas dans le menu burger,
-  * alors le menu sera fermé. Ceci est vérifié en utilisant la méthode `contains` sur l'élément natif du menu burger.
-  * 
-  * @param event L'objet MouseEvent associé au clic du document.
-  */
-  @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
-  @ViewChild('menuBurger') menuBurger!: ElementRef;
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: MouseEvent) {
-    if (this.burgerMenuOpened && !this.menuBurger.nativeElement.contains(event.target)) {
-      this.burgerMenuOpened = false;
-    }
-  }
-
-  /**
-  * Permet la navigation vers différentes sections de la page en utilisant un défilement fluide.
-  * Si le menu burger est ouvert, il est d'abord fermé avant de procéder au défilement.
-  * La méthode recherche l'élément de section par son identifiant. Si l'élément est trouvé, elle calcule la position de l'élément
-  * en tenant compte de la hauteur fixe de l'en-tête et déplace le défilement à cette position avec un comportement fluide.
-  *
-  * @param sectionId L'identifiant de l'élément HTML vers lequel défiler.
-  */
-  scrollToSection(sectionId: string): void {
-    if (this.burgerMenuOpened) {
-      this.burgerMenuOpened = !this.burgerMenuOpened;
+    ngOnInit() {
+        this.loadIcons();
     }
 
-    setTimeout(() => {
-      const section = document.getElementById(sectionId);
-      if (section) {
-        const sectionTop = section.getBoundingClientRect().top + window.scrollY;
-        const headerHeight = 64;
-        const position = sectionTop - headerHeight;
-        window.scrollTo({ top: position, behavior: 'smooth' });
-      }
-    }, 50);
-  }
-
-  /**
-  * Sert à ouvrir ou fermer le menu burger en inversant l'état actuel du menu. 
-  * Elle arrête également la propagation de l'événement de clic pour éviter des interactions indésirables avec d'autres éléments de l'interface utilisateur.
-  * 
-  * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode. Utilisé pour arrêter la propagation de l'événement.
-  */
-  toggleBurgerMenu(event: MouseEvent): void {
-    event.stopPropagation();
-    this.burgerMenuOpened = !this.burgerMenuOpened;
-  }
-
-  /**
-  * Prépare et envoie un email en utilisant le service commun.
-  * Si l'envoi de l'email réussit, on réinitialise les champs de saisie.
-  *
-  * @returns {Promise<void>} Une promesse qui se résout une fois que l'email a été envoyé et que les 
-  * champs de saisie ont été réinitialisés en cas de succès.
-  */
-  async sendMail(): Promise<void> {
-    this.getDataIntoDictionary();
-    if (await this.commonService.sendMail(this.inputLabelMap, false)) {
-      this.resetInputFields();
+    /**
+    * Navigue vers un composant spécifié.
+    *
+    * @param {string} composant - Le nom du composant vers lequel naviguer. Cela doit être le chemin ou
+    *                             la route associée au composant cible dans la configuration de routage Angular.
+    */
+    navigateTo(component: string) {
+        this.router.navigate([component]).then(() => {
+            window.scrollTo(0, 0);
+        });
     }
-  }
 
-  /**
-  * Parcourt les champs de saisie dans le HTML et mappe leurs valeurs à leurs labels correspondants.
-  * La méthode utilise `inputFields` pour obtenir une liste des éléments de saisie. Pour chaque champ de saisie, elle récupère
-  * le label associé en utilisant son attribut 'id'. Si un label est trouvé pour une valeur de champ, la méthode les mappent dans `inputLabelMap`.
-  */
-  private getDataIntoDictionary() {
-    this.inputFields.forEach(input => {
-      const label = document.querySelector(`label[for="${input.nativeElement.id}"]`);
-      if (label) {
-        this.inputLabelMap.set(label.textContent!.trim(), input.nativeElement.value);
-      }
-    });
-  }
+    /**
+    * Gère les clics à l'extérieur du menu burger pour fermer le menu.
+    * 
+    * Cette méthode est déclenchée par un écouteur d'événements qui surveille tous les clics dans le document.
+    * Si le menu burger est ouvert et que le clic n'est pas dans le menu burger,
+    * alors le menu sera fermé. Ceci est vérifié en utilisant la méthode `contains` sur l'élément natif du menu burger.
+    * 
+    * @param event L'objet MouseEvent associé au clic du document.
+    */
+    @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
+    @ViewChild('menuBurger') menuBurger!: ElementRef;
+    @HostListener('document:click', ['$event'])
+    onClickOutside(event: MouseEvent) {
+        if (this.burgerMenuOpened && !this.menuBurger.nativeElement.contains(event.target)) {
+            this.burgerMenuOpened = false;
+        }
+    }
 
-  /**
-  * Réinitialise les valeurs de tous les champs de saisie marqués avec la directive locale #inputField.
-  * En l'occurence, la méthode permet de réinitialiser la valeur des champs de l'envoi de mail.
-  */
-  resetInputFields() {
-    this.inputFields.forEach(field => {
-      if (field.nativeElement instanceof HTMLInputElement || field.nativeElement instanceof HTMLTextAreaElement) {
-        field.nativeElement.value = '';
-      }
-    });
-  }
+    /**
+    * Permet la navigation vers différentes sections de la page en utilisant un défilement fluide.
+    * Si le menu burger est ouvert, il est d'abord fermé avant de procéder au défilement.
+    * La méthode recherche l'élément de section par son identifiant. Si l'élément est trouvé, elle calcule la position de l'élément
+    * en tenant compte de la hauteur fixe de l'en-tête et déplace le défilement à cette position avec un comportement fluide.
+    *
+    * @param sectionId L'identifiant de l'élément HTML vers lequel défiler.
+    */
+    scrollToSection(sectionId: string): void {
+        if (this.burgerMenuOpened) {
+            this.burgerMenuOpened = !this.burgerMenuOpened;
+        }
 
+        setTimeout(() => {
+            const section = document.getElementById(sectionId);
+            if (section) {
+                const sectionTop = section.getBoundingClientRect().top + window.scrollY;
+                const headerHeight = 64;
+                const position = sectionTop - headerHeight;
+                window.scrollTo({ top: position, behavior: 'smooth' });
+            }
+        }, 50);
+    }
+
+    /**
+    * Sert à ouvrir ou fermer le menu burger en inversant l'état actuel du menu. 
+    * Elle arrête également la propagation de l'événement de clic pour éviter des interactions indésirables avec d'autres éléments de l'interface utilisateur.
+    * 
+    * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode. Utilisé pour arrêter la propagation de l'événement.
+    */
+    toggleBurgerMenu(event: MouseEvent): void {
+        event.stopPropagation();
+        this.burgerMenuOpened = !this.burgerMenuOpened;
+    }
+
+    /**
+    * Prépare et envoie un email en utilisant le service commun.
+    * Si l'envoi de l'email réussit, on réinitialise les champs de saisie.
+    *
+    * @returns {Promise<void>} Une promesse qui se résout une fois que l'email a été envoyé et que les 
+    * champs de saisie ont été réinitialisés en cas de succès.
+    */
+    async sendMail(): Promise<void> {
+        this.getDataIntoDictionary();
+        if (await this.commonService.sendMail(this.inputLabelMap, false)) {
+            this.resetInputFields();
+        }
+    }
+
+    /**
+    * Parcourt les champs de saisie dans le HTML et mappe leurs valeurs à leurs labels correspondants.
+    * La méthode utilise `inputFields` pour obtenir une liste des éléments de saisie. Pour chaque champ de saisie, elle récupère
+    * le label associé en utilisant son attribut 'id'. Si un label est trouvé pour une valeur de champ, la méthode les mappent dans `inputLabelMap`.
+    */
+    private getDataIntoDictionary() {
+        this.inputFields.forEach(input => {
+            const label = document.querySelector(`label[for="${input.nativeElement.id}"]`);
+            if (label) {
+                this.inputLabelMap.set(label.textContent!.trim(), input.nativeElement.value);
+            }
+        });
+    }
+
+    /**
+    * Réinitialise les valeurs de tous les champs de saisie marqués avec la directive locale #inputField.
+    * En l'occurence, la méthode permet de réinitialiser la valeur des champs de l'envoi de mail.
+    */
+    resetInputFields() {
+        this.inputFields.forEach(field => {
+            if (field.nativeElement instanceof HTMLInputElement || field.nativeElement instanceof HTMLTextAreaElement) {
+                field.nativeElement.value = '';
+            }
+        });
+    }
+
+    /**
+   * Met à jour la liste des icônes visibles en fonction de la page actuelle.
+   * Cette méthode calcule les icônes à afficher pour la page en cours
+   * en utilisant l'index de début basé sur `currentPage` et `iconsPerPage`.
+   */
+    updateVisibleIcons(): void {
+        const startIndex = this.currentPage * this.iconsPerPage;
+        this.visibleIcons = this.icons.slice(startIndex, startIndex + this.iconsPerPage);
+    }
+
+    /**
+     * Passe à la page suivante des icônes, si elle existe.
+     * Incrémente le numéro de page `currentPage` et met à jour les icônes visibles.
+     * Cette méthode ne fait rien si la page actuelle est la dernière disponible.
+     */
+    nextPage(): void {
+        if ((this.currentPage + 1) * this.iconsPerPage < this.icons.length) {
+            this.currentPage++;
+            this.updateVisibleIcons();
+        }
+    }
+
+    /**
+     * Revient à la page précédente des icônes, si elle existe.
+     * Décrémente le numéro de page `currentPage` et met à jour les icônes visibles.
+     * Cette méthode ne fait rien si la page actuelle est la première.
+     */
+    prevPage(): void {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+            this.updateVisibleIcons();
+        }
+    }
 }
