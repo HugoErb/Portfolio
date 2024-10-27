28af873dec78270db2580698fe15e4d95aa83633
HugoErb <erb.hugo@yahoo.fr>
2024-10-27 15:00:59 +0100

    adaptation de la route pour le nouveau component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LegalInformationComponent } from './legal-information/legal-information.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full'}
+  { path: 'legal-information', component: LegalInformationComponent },
+  
 ];
