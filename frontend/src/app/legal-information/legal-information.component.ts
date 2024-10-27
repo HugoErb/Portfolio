06a568621ee1efc8326c33007bcba1fce8aa6046
HugoErb <erb.hugo@yahoo.fr>
2024-10-27 15:04:25 +0100

    construction du composant legal info avec celui de safesec

--- frontend/src/app/legal-information/legal-information.component.ts
+++ frontend/src/app/legal-information/legal-information.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-legal-information',
-  standalone: true,
-  imports: [],
-  templateUrl: './legal-information.component.html'
+    selector: 'app-legal-infos',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './legal-information.component.html'
 })
 export class LegalInformationComponent {
 
+    constructor(private router: Router) {
+    }
+
+    burgerMenuOpened: boolean = false;
+
+    /**
+    * Gère les clics à l'extérieur du menu burger pour fermer le menu.
+    * 
+    * Cette méthode est déclenchée par un écouteur d'événements qui surveille tous les clics dans le document.
+    * Si le menu burger est ouvert et que le clic n'est pas dans le menu burger,
+    * alors le menu sera fermé. Ceci est vérifié en utilisant la méthode `contains` sur l'élément natif du menu burger.
+    * 
+    * @param event L'objet MouseEvent associé au clic du document.
+    */
+    @ViewChild('menuContainerRef') menuContainerRef!: ElementRef;
+    @ViewChild('menuBurger') menuBurger!: ElementRef;
+    @HostListener('document:click', ['$event'])
+    onClickOutside(event: MouseEvent) {
+        if (this.burgerMenuOpened && !this.menuBurger.nativeElement.contains(event.target)) {
+            this.burgerMenuOpened = false;
+        }
+    }
+
+    /**
+    * Navigue vers un composant spécifié et, optionnellement, fait défiler vers une section au sein de ce composant.
+    *
+    * @param {string} component - Le nom du composant vers lequel naviguer. Cela doit être le chemin ou
+    *                             la route associée au composant cible dans la configuration de routage Angular.
+    * @param {string} section - La section au sein du composant cible vers laquelle l'utilisateur doit être redirigé.
+    *                           Ce paramètre est optionnel et est utilisé pour indiquer une section ou
+    *                           un fragment spécifique au sein du composant.
+    */
+    navigateTo(component: string, section: string) {
+        this.router.navigate([component, { redirectionSection: section }]);
+    }
+
+    /**
+    * Sert à ouvrir ou fermer le menu burger en inversant l'état actuel du menu. 
+    * Elle arrête également la propagation de l'événement de clic pour éviter des interactions indésirables avec d'autres éléments de l'interface utilisateur.
+    * 
+    * @param {MouseEvent} event - L'événement de clic qui a déclenché l'appel de la méthode. Utilisé pour arrêter la propagation de l'événement.
+    */
+    toggleBurgerMenu(event: MouseEvent): void {
+        event.stopPropagation();
+        this.burgerMenuOpened = !this.burgerMenuOpened;
+    }
 }
