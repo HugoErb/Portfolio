c3330e1ef27164cb16716c98f0e9e8a8923b1653
HugoErb <erb.hugo@yahoo.fr>
2024-10-27 14:59:16 +0100

    création du composant legal info

---
+++ frontend/src/app/legal-information/legal-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './legal-information.component.html'
+})
+export class LegalInformationComponent {
+
+}
