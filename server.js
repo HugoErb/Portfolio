8980b995470999a47594770b20d9c9b18b1ebdde
HugoErb <erb.hugo@yahoo.fr>
2024-11-02 15:56:15 +0100

    modif conf

--- server.js
+++ server.js
@@ -78,4 +78,3 @@ if (process.env.NODE_ENV !== 'dev') {
   });
 }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
