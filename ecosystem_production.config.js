5f3968cc26d6b0214e9daf9440db1cc059605262
HugoErb <erb.hugo@yahoo.fr>
2024-11-02 15:51:11 +0100

    modif port

--- ecosystem_production.config.js
+++ ecosystem_production.config.js
@@ -6,7 +6,7 @@ module.exports = {
             watch: true,
             env: {
                 "NODE_ENV": "production",
-                "PORT": 3003
+                "PORT": 3000
             }
         }
     ]
