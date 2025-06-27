a783e17916c0bc28751d3c86131d1123be80db21
HugoErb <erb.hugo@yahoo.fr>
2025-06-27 23:56:09 +0200

    fix server.js + ajout des domaines trusted

--- frontend/src/app/common.service.ts
+++ frontend/src/app/common.service.ts
@@ -16,6 +16,18 @@ export class CommonService {
 
     constructor(private mailService: MailService) { }
 
+    // Liste blanche des domaines populaires considérés comme fiables
+	private trustedEmailDomains = new Set([
+		'gmail.com',
+		'hotmail.com',
+		'outlook.com',
+		'yahoo.com',
+		'yahoo.fr',
+		'live.com',
+		'protonmail.com',
+		'icloud.com',
+	]);
+
     /**
     * Filtre et formate la saisie d'un numéro de téléphone dans un champ de saisie HTML.
     * Seules les valeurs numériques sont conservées, et un espace est ajouté tous les deux chiffres.
@@ -114,12 +126,17 @@ export class CommonService {
                 return false;
             }
 
-            // Vérification pour l'email
+            // Vérifications pour l'email
             if (lowerCaseLabel.includes('email')) {
                 if (!emailRegex.test(trimmedValue)) {
                     this.showValidationError('Le format de l\'adresse email est invalide.');
                     return false;
                 } else {
+                    // Vérification du domaine de l'email
+                    const domain = trimmedValue.split('@')[1]?.toLowerCase();
+                    if (this.trustedEmailDomains.has(domain)) {
+                        continue;
+                    }
                     const isEmailValid = await this.checkEmailValidity(trimmedValue);
                     if (!isEmailValid) {
                         this.showValidationError('Le domaine de l\'adresse email n\'est pas accepté.');
--- server.js
+++ server.js
@@ -21,68 +21,68 @@ app.use(express.json());
 
 // Limitation du nombre de requêtes sur l'endpoint /send-mail pour prévenir les abus
 const limiter = rateLimit({
-  windowMs: 24 * 60 * 60 * 1000, // fenêtre de 24 heures
-  max: 5,                       // maximum 5 requêtes par fenêtre par adresse IP
-  message: "Trop de requêtes depuis cette IP, veuillez réessayer demain." // réponse en cas de dépassement
+    windowMs: 24 * 60 * 60 * 1000, // fenêtre de 24 heures
+    max: 5,                       // maximum 5 requêtes par fenêtre par adresse IP
+    message: "Trop de requêtes depuis cette IP, veuillez réessayer demain." // réponse en cas de dépassement
 });
 app.use('/send-mail', limiter);
 
 // Configuration CORS : n'accepte que les requêtes POST depuis votre frontend
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'dev'
-    ? 'http://localhost:4200'        // en développement
-    : 'https://hugoeribon.fr',      // en production
-  methods: ['POST'],                // n'autoriser que la méthode POST
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+    origin: process.env.NODE_ENV === 'dev'
+        ? 'http://localhost:4200'        // en développement
+        : 'https://hugoeribon.fr',      // en production
+    methods: ['POST'],                // n'autoriser que la méthode POST
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 app.use(cors(corsOptions));
 
 // Point de terminaison pour l'envoi d'e-mails
 app.post('/send-mail', async (req, res) => {
-  // Récupération des données envoyées depuis le formulaire
-  const { name, email, phoneNumber, message } = req.body;
+    // Récupération des données envoyées depuis le formulaire
+    const [name, email, phoneNumber, message] = Object.values(req.body);
 
-  // Vérification de la présence de tous les champs requis
-  if (!name || !email || !phoneNumber || !message) {
-    return res.status(400).json({ error: 'Champs manquants.' });
-  }
+    // Vérification de la présence de tous les champs requis
+    if (!name || !email || !phoneNumber || !message) {
+        return res.status(400).json({ error: 'Champs manquants.' });
+    }
 
-  // Préparation du message à envoyer à l'administrateur
-  const msgToMe = {
-    to: process.env.SENDER_EMAIL,
-    from: process.env.SENDER_EMAIL,
-    replyTo: email, // l'utilisateur pourra répondre directement
-    subject: `Nouveau message de ${name}`,
-    text: `Nom : ${name}\nEmail : ${email}\nNuméro de tél : ${phoneNumber}\n\nMessage :\n${message}`
-  };
+    // Préparation du message à envoyer à l'administrateur
+    const msgToMe = {
+        to: process.env.ADMIN_EMAIL,
+        from: process.env.SENDER_EMAIL,
+        replyTo: email, // l'utilisateur pourra répondre directement
+        subject: `Nouveau message de ${name}`,
+        text: `Nom : ${name}\nEmail : ${email}\nNuméro de tél : ${phoneNumber}\n\nMessage :\n${message}`
+    };
 
-  // Préparation du message de confirmation à l'utilisateur
-  const msgToUser = {
-    to: email,
-    from: process.env.SENDER_EMAIL,
-    subject: 'Eribon Hugo - Réception de votre message',
-      text: "Bonjour !\n\nJ'ai bien reçu votre message. Je vais l'étudier attentivement et j'y répondrai dans les plus brefs délais.\nEn attendant, vous pouvez continuer de parcourir mon site internet, ou visiter mon Linkedin. Merci pour votre confiance !\n\nERIBON Hugo\n\nCet e-mail a été envoyé automatiquement, merci de ne pas y répondre."
-  };
+    // Préparation du message de confirmation à l'utilisateur
+    const msgToUser = {
+        to: email,
+        from: process.env.SENDER_EMAIL,
+        subject: 'Eribon Hugo - Réception de votre message',
+        text: "Bonjour !\n\nJ'ai bien reçu votre message. Je vais l'étudier attentivement et j'y répondrai dans les plus brefs délais.\nEn attendant, vous pouvez continuer de parcourir mon site internet, ou visiter mon Linkedin. Merci pour votre confiance !\n\nERIBON Hugo\n\nCet e-mail a été envoyé automatiquement, merci de ne pas y répondre."
+    };
 
-  try {
-    // Envoi simultané des deux e-mails (utilisateur et administrateur)
-    await sgMail.send([msgToMe, msgToUser]);
-    return res.status(200).json({ message: 'Emails envoyés avec succès.' });
-  } catch (err) {
-    // En cas d'erreur lors de l'envoi
-    console.error(err);
-    return res.status(500).json({ error: err.message });
-  }
+    try {
+        // Envoi simultané des deux e-mails (utilisateur et administrateur)
+        await sgMail.send([msgToMe, msgToUser]);
+        return res.status(200).json({ message: 'Emails envoyés avec succès.' });
+    } catch (err) {
+        // En cas d'erreur lors de l'envoi
+        console.error(err);
+        return res.status(500).json({ error: err.message });
+    }
 });
 
 // Service des fichiers statiques du frontend en production
 if (process.env.NODE_ENV !== 'dev') {
-  const distDir = __dirname + '/dist/';
-  app.use(express.static(distDir));                // Sert les fichiers du dossier dist/
-  app.get('*', (req, res) => {
-    res.sendFile(distDir + 'index.html');          // Route toutes les requêtes vers index.html pour Angular
-  });
+    const distDir = __dirname + '/dist/';
+    app.use(express.static(distDir));                // Sert les fichiers du dossier dist/
+    app.get('*', (req, res) => {
+        res.sendFile(distDir + 'index.html');          // Route toutes les requêtes vers index.html pour Angular
+    });
 }
 
 // Démarrage du serveur Express
