05fea93564a0f22fdc2eec0d27a56d8dbea49dd9
HugoErb <erb.hugo@yahoo.fr>
2025-09-28 21:00:04 +0200

    refacto + nouvelles images + changement d'ordre des projets

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { LegalInformationComponent } from './legal-information/legal-information.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'legal-information', component: LegalInformationComponent },
-  
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: 'home', component: HomeComponent },
+	{ path: 'legal-information', component: LegalInformationComponent },
 ];
