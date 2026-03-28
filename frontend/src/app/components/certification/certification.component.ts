09383817eda4b31931b55f4cd52ca74e8d6d9bb6
HugoErb <erb.hugo@yahoo.fr>
2026-03-28 15:24:59 +0100

    fix: move provideZoneChangeDetection to appConfig, clean imports

--- frontend/src/app/components/certification/certification.component.ts
+++ frontend/src/app/components/certification/certification.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input } from '@angular/core';
-
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
     selector: 'app-certification',
